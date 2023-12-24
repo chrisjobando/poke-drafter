2e81f3de0d9d32f2f3cf011ece9e61685a8beb85
Christopher Obando <chrisjobando@gmail.com>
2023-12-24 10:38:59 -0500

    add common string utils

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+export function lowercaseString(str: string) {
+	return str.toLowerCase();
+}
+
+export function capitalizeFirstLetter(str: string) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+}
