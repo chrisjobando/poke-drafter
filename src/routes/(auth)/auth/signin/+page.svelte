f05c4af575f68a341440d9d6eb271881dd2a4f15
Christopher Obando <chrisjobando@gmail.com>
2023-12-24 16:59:07 -0500

    better signin page

--- src/routes/(auth)/auth/signin/+page.svelte
+++ src/routes/(auth)/auth/signin/+page.svelte
@@ -1,18 +1,64 @@
+<script lang="ts">
+	// Forms
+	import { enhance } from '$app/forms';
+	// Components
+	import Alert from '$lib/components/form/Alert.svelte';
+	import EmailInput from '$lib/components/form/EmailInput.svelte';
+	import PasswordInput from '$lib/components/form/PasswordInput.svelte';
+	// Interfaces
+	import type { ILoginErrors } from '$lib/db/schema/UserSchema';
+
+	export let form;
+
+	// Form Validation
+	let formErrors: ILoginErrors | undefined;
+
+	$: if (form) {
+		if (form.success) {
+			// Clear loading
+			formErrors = undefined;
+		} else {
+			if ('errors' in form && form.errors) {
+				formErrors = form.errors as ILoginErrors;
+			}
+		}
+	}
+</script>
+
 <svelte:head>
 	<title>Sign In</title>
+	<meta name="description" content="Sign in to PokeDrafter" />
 </svelte:head>
 
 <div class="card">
 	<h1 class="card-title">Sign In</h1>
 
-	<form class="card-body">
-		<label class="label-text" for="email">Email</label>
-		<input class="input input-bordered" name="email" type="email" />
+	<form
+		class="card-body"
+		method="POST"
+		use:enhance={() => {
+			return async ({ update }) => {
+				// Prevent clearing form inputs on submit
+				await update({ reset: false });
+			};
+		}}
+	>
+		{#if form && form.message.length > 0}
+			<Alert
+				message={form.message}
+				type={form.success ? 'success' : 'warning'}
+				onDestroyCallback={() => {
+					if (form) {
+						form.message = '';
+					}
+				}}
+			/>
+		{/if}
 
-		<label class="label-text" for="password">Password</label>
-		<input class="input input-bordered" name="password" type="password" />
+		<EmailInput required errorMessage={formErrors?.email} />
+		<PasswordInput required label="Password" name="password" errorMessage={formErrors?.password} />
 
-		<a href="/" class="btn btn-primary">Submit</a>
+		<button class="btn btn-primary" type="submit">Submit</button>
 		<p>
 			Don't have an account? <a class="text-primary" href="/auth/signup">Create a new account</a>
 		</p>
