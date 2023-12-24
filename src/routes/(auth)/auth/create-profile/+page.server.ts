7de58772375093a14a1f3c3f2cfa5ce6bad4ccf5
Christopher Obando <chrisjobando@gmail.com>
2023-12-24 16:58:51 -0500

    create profile page

--- src/routes/(auth)/auth/create-profile/+page.server.ts
+++ src/routes/(auth)/auth/create-profile/+page.server.ts
@@ -1,9 +1,53 @@
-import { redirect } from '@sveltejs/kit';
+import { CreateTrainerSchema } from '$lib/db/schema/UserSchema.js';
+import { fault, formatError } from '$lib/db/utils.js';
+import { AuthApiError } from '@supabase/supabase-js';
+import { fail, redirect } from '@sveltejs/kit';
+import { ZodError } from 'zod';
 
-export const load = async ({ locals: { getSession } }) => {
-  const session = await getSession();
+import type { Actions } from './$types.js';
 
-  if (!session) {
-    throw redirect(307, '/auth/signup');
-  }
+export const load = async ({ locals: { getTrainer } }) => {
+	const trainerData = await getTrainer();
+
+	if (trainerData) {
+		throw redirect(307, '/');
+	}
+};
+
+export const actions: Actions = {
+	default: async ({ request, locals: { supabase, getSession } }) => {
+		const formData = await request.formData();
+		const display_name = formData.get('name') as string;
+
+		try {
+			CreateTrainerSchema.parse({ name });
+		} catch (err) {
+			if (err instanceof ZodError) {
+				const errors = formatError(err);
+				return fail(400, fault('', { errors, name }));
+			}
+		}
+
+		const session = await getSession();
+
+		if (!session) {
+			throw redirect(307, '/auth/signup');
+		}
+
+		const auth_id = session.user.id;
+
+		const { error: createTrainerError } = await supabase
+			.from('trainers')
+			.insert({ auth_id, display_name });
+
+		if (createTrainerError) {
+			if (createTrainerError instanceof AuthApiError && createTrainerError.status === 400) {
+				return fail(400, fault('Invalid credentials.', { display_name }));
+			}
+
+			return fail(500, fault(createTrainerError.message, { display_name }));
+		}
+
+		return redirect(307, '/');
+	}
 };
--- src/routes/(auth)/auth/create-profile/+page.svelte
+++ src/routes/(auth)/auth/create-profile/+page.svelte
@@ -1,14 +1,36 @@
+<script lang="ts">
+	// Forms
+	import { enhance } from '$app/forms';
+	// Components
+	import TextInput from '$lib/components/form/TextInput.svelte';
+</script>
+
 <svelte:head>
 	<title>Create Profile</title>
+	<meta name="description" content="Create your PokeDrafter profile" />
 </svelte:head>
 
 <div class="card">
 	<h1 class="card-title">Create Profile</h1>
 
-	<form class="card-body">
-		<label class="label-text" for="name">Name</label>
-		<input class="input input-bordered" name="name" type="text" />
+	<form
+		class="card-body"
+		method="POST"
+		use:enhance={() => {
+			return async ({ update }) => {
+				// Prevent clearing form input on submit
+				await update({ reset: false });
+			};
+		}}
+	>
+		<TextInput
+			required
+			autocomplete="given-name"
+			label="Name"
+			name="name"
+			placeholder="Ash Ketchum"
+		/>
 
-		<a href="/" class="btn btn-primary">Let's Go!</a>
+		<button class="btn btn-primary" type="submit">Let's Go!</button>
 	</form>
 </div>
