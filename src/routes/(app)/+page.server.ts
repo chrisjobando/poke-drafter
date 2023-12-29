85899b30ebfdf30ea5c1e45e972aa5981d473539
Christopher Obando <chrisjobando@gmail.com>
2023-12-29 12:06:59 -0500

    remove previous testing

--- src/routes/(app)/+page.server.ts
+++ src/routes/(app)/+page.server.ts
@@ -1,106 +1,18 @@
-import { fault } from '$lib/db/utils.js';
 import { capitalizeFirstLetter, lowercaseString } from '$lib/utils.js';
-import { AuthApiError } from '@supabase/supabase-js';
-import { fail, redirect } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 import { PokemonClient } from 'pokenode-ts';
 
-import type { INewPokemon, IPokemon, ITeam } from '$lib/db/schema/DatabaseTypes.js';
+// Interfaces
+import type { IPokemon } from '$lib/db/schema/DatabaseTypes.js';
 import type { Pokemon } from 'pokenode-ts';
 import type { Actions } from './$types.js';
 
-interface IHomePageLoadData {
-	trainer_name?: string;
-	current_round?: number;
-	current_gym?: string;
-	current_pick?: string;
-	trainer_team?: ITeam;
-	trainer_pokemon?: IPokemon[];
-}
-
-export const load = async ({ locals: { supabase, getDraft, getTrainer } }) => {
-	const draftLoadData: IHomePageLoadData = {};
+export const load = async ({ locals: { getTrainer } }) => {
 	const trainerData = await getTrainer();
 
 	if (!trainerData) {
 		throw redirect(307, '/auth/create-profile');
 	}
-
-	draftLoadData.trainer_name = trainerData.display_name;
-
-	const draftData = await getDraft();
-
-	if (!draftData || !draftData.active_gym || !draftData.is_active) {
-		return draftLoadData;
-	}
-
-	draftLoadData.current_round = draftData.round_number;
-
-	const { data: current_gym } = await supabase
-		.from('gyms')
-		.select('gym_name,id')
-		.eq('id', draftData.active_gym)
-		.maybeSingle();
-
-	if (!current_gym) {
-		return draftLoadData;
-	} else {
-		draftLoadData.current_gym = current_gym.gym_name;
-		if (!draftData.active_trainer) {
-			return draftLoadData;
-		}
-	}
-
-	const { data: current_pick } = await supabase
-		.from('trainers')
-		.select('display_name')
-		.eq('id', draftData.active_trainer)
-		.maybeSingle();
-
-	if (!current_pick) {
-		return draftLoadData;
-	}
-
-	draftLoadData.current_pick = current_pick.display_name;
-
-	const { data: current_team } = await supabase
-		.from('teams')
-		.select('*')
-		.eq('trainer_id', draftData.active_trainer)
-		.eq('gym_id', current_gym.id)
-		.maybeSingle();
-
-	let trainer_team: ITeam | null = current_team;
-
-	if (!current_team) {
-		const { data: new_team } = await supabase
-			.from('teams')
-			.insert({ gym_id: draftData.active_gym, trainer_id: trainerData.id })
-			.select('*')
-			.single();
-
-		if (new_team) {
-			trainer_team = new_team;
-		}
-	}
-
-	if (!trainer_team) {
-		return draftLoadData;
-	}
-
-	draftLoadData.trainer_team = trainer_team;
-
-	const { data: trainer_pokemon } = await supabase
-		.from('pokemon')
-		.select('*')
-		.eq('team_id', trainer_team.id);
-
-	if (!trainer_pokemon) {
-		return draftLoadData;
-	}
-
-	draftLoadData.trainer_pokemon = trainer_pokemon;
-
-	return draftLoadData;
 };
 
 export const actions: Actions = {
@@ -115,7 +27,6 @@ export const actions: Actions = {
 
 		try {
 			pokemonData = await pokemonApi.getPokemonByName(lower_pokemon_name);
-			console.log('cobando ~ file: +page.server.ts:118 ~ pokemonData:', pokemonData);
 		} catch (error) {
 			console.error(error);
 		}
@@ -137,70 +48,6 @@ export const actions: Actions = {
 		return {
 			selectedPokemon
 		};
-	},
-	draftPokemon: async ({ request, locals: { supabase, getDraft, getTeam, getTrainer } }) => {
-		const formData = await request.formData();
-		const pokemon_data = JSON.parse(formData.get('pokemon_data') as string) as IPokemon;
-
-		let draft_team = await getTeam();
-
-		if (!draft_team) {
-			const trainerData = await getTrainer();
-			const draftData = await getDraft();
-
-			if (!trainerData) {
-				throw redirect(307, '/auth/create-profile');
-			}
-
-			if (!draftData || !draftData.active_gym || !draftData.is_active) {
-				// Should never occur
-				return fail(400, fault('There is no active draft to join.'));
-			}
-
-			// Create a new team
-			const { data: new_team, error: createTeamError } = await supabase
-				.from('teams')
-				.insert({ gym_id: draftData.active_gym, trainer_id: trainerData.id })
-				.select('*')
-				.single();
-
-			if (createTeamError) {
-				if (createTeamError instanceof AuthApiError && createTeamError.status === 400) {
-					return fail(400, fault('Team already exists.'));
-				}
-
-				return fail(500, fault(createTeamError.message));
-			}
-
-			if (new_team) {
-				draft_team = new_team;
-			} else {
-				// Should never occur
-				return fail(400, fault('Trainer does not have a team to add pokemon to.'));
-			}
-		}
-
-		const drafted_pokemon: INewPokemon = {
-			avatar_url: pokemon_data.avatar_url,
-			name: pokemon_data.name,
-			pokemon_id: pokemon_data.id,
-			team_id: draft_team.id,
-			type: pokemon_data.type,
-			weakness: []
-		};
-
-		const { error: draftPokemonError } = await supabase
-			.from('pokemon')
-			.insert(drafted_pokemon)
-			.single();
-
-		if (draftPokemonError) {
-			if (draftPokemonError instanceof AuthApiError && draftPokemonError.status === 400) {
-				return fail(400, fault('Pokemon already drafted.'));
-			}
-
-			return fail(500, fault(draftPokemonError.message));
-		}
 	}
 };
 ``;
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,45 +1,22 @@
 <script lang="ts">
 	// Forms
-	import { enhance } from '$app/forms';
 	// Components
-	import PokemonSearch from '$lib/components/PokemonSearch.svelte';
-	import SelectedPokemonDisplay from '$lib/components/draft/SelectedPokemonDisplay.svelte';
-	import TrainerTeamDisplay from '$lib/components/draft/TrainerTeamDisplay.svelte';
+	import { DraftDisplay, PokedexDisplay, TeamDisplay } from '$lib/components/draft';
+	import { Button } from 'flowbite-svelte';
 	// Interfaces
-	import type { IPokemon } from '$lib/db/schema/DatabaseTypes';
 	import type { ActionData, PageData } from './$types';
 
 	let { data, form } = $props<{ data: PageData; form: ActionData }>();
 
-	const {
-		current_gym: initial_gym,
-		current_pick: initial_trainer,
-		current_round: initial_round,
-		trainer_name,
-		trainer_team,
-		trainer_pokemon: initial_pokemon,
-		supabase
-	} = data;
+	const { supabase } = data;
 
 	// State
-	let current_gym = $state<string | undefined>(initial_gym);
-	let current_pick = $state<string | undefined>(initial_trainer);
-	let current_round = $state<number | undefined>(initial_round);
-
-	let searchQuery = $state<string>('');
-	let filteredNames = $state<string[]>([]);
-
-	let selectedPokemon = $state<IPokemon | undefined>();
-	let trainer_pokemon = $state<IPokemon[] | undefined>(initial_pokemon);
 
 	// Form Ref
-	let formRef = $state<HTMLFormElement | undefined>();
 
 	// Search Results
 	$effect(() => {
-		if (form && form.selectedPokemon) {
-			selectedPokemon = form.selectedPokemon;
-		}
+		//
 	});
 
 	// Subscriptions
@@ -51,40 +28,7 @@
 		async (payload) => {
 			const updatedDraftData = payload.new;
 
-			if (updatedDraftData.round_number) current_round = updatedDraftData.round_number;
-
-			const { data: updated_gym } = await supabase
-				.from('gyms')
-				.select('gym_name')
-				.eq('id', updatedDraftData.active_gym)
-				.maybeSingle();
-
-			if (updated_gym) current_gym = updated_gym.gym_name;
-
-			const { data: updated_trainer } = await supabase
-				.from('trainers')
-				.select('display_name')
-				.eq('id', updatedDraftData.active_trainer)
-				.maybeSingle();
-
-			if (updated_trainer) current_pick = updated_trainer.display_name;
-		}
-	);
-
-	draftChannel.on(
-		'postgres_changes',
-		{ event: 'INSERT', schema: 'public', table: 'pokemon' },
-		async (payload) => {
-			const newPokemonData = payload.new as IPokemon;
-
-			if (trainer_team && trainer_pokemon && newPokemonData.team_id === trainer_team.id) {
-				trainer_pokemon?.push(newPokemonData);
-
-				// Reset form values
-				searchQuery = '';
-				filteredNames = [];
-				selectedPokemon = undefined;
-			}
+			console.log('cobando ~ file: +page.svelte:29 ~ updatedDraftData:', updatedDraftData);
 		}
 	);
 
@@ -95,61 +39,16 @@
 	<title>Draft Tool</title>
 </svelte:head>
 
-<h1 class="text-2xl">Welcome, {trainer_name}</h1>
+<div class="grid min-h-screen grid-cols-12 gap-5">
+	<DraftDisplay />
 
-{#if current_gym}
-	<div role="alert" class="alert alert-error mt-4">
-		<span class="text-2xl font-bold">Round: {current_round}</span>
-		<span class="ml-auto text-2xl font-bold">Current Draft: {current_gym} Gym</span>
-	</div>
+	<TeamDisplay />
 
-	{#if trainer_name === current_pick}
-		<h2 class="mt-4 text-xl font-bold">Your Pick!</h2>
+	<PokedexDisplay />
 
-		<div class="mx-auto flex h-[400px] w-64 flex-col justify-center">
-			{#if selectedPokemon}
-				<SelectedPokemonDisplay bind:selectedPokemon />
-			{/if}
-		</div>
+	<div class="bg-primary-500 h-32 w-full"></div>
 
-		<form
-			action="?/previewPokemon"
-			method="post"
-			bind:this={formRef}
-			use:enhance={({ formData }) => {
-				formData.set('pokemon_name', searchQuery);
-
-				return async ({ update }) => {
-					// Prevent clearing form inputs on submit
-					await update({ reset: false });
-				};
-			}}
-		>
-			<PokemonSearch
-				name="pokemon_name"
-				placeholder="Enter name here"
-				bind:filteredNames
-				bind:searchQuery
-				bind:formRef
-			/>
-		</form>
-
-		<h2 class="mt-4 text-xl">Your Team:</h2>
-
-		<TrainerTeamDisplay bind:trainer_pokemon />
-	{:else}
-		<h2 class="mt-4 text-xl">Current Pick: <span class="font-bold">{current_pick}</span></h2>
-	{/if}
-{:else}
-	<h2 class="mt-4">No active draft.</h2>
-{/if}
-
-<style lang="postcss">
-	.avatar p {
-		display: none;
-	}
-
-	.avatar:hover p {
-		display: block;
-	}
-</style>
+	<form method="POST" action="/auth/signout">
+		<Button type="submit">Sign out</Button>
+	</form>
+</div>
