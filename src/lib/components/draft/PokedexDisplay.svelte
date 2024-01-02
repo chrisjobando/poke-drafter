dda8e38fabf8384013e28eba3c73ecc9305b339c
Christopher Obando <chrisjobando@gmail.com>
2024-01-02 10:00:27 -0500

    update pokedex to add draft and preview

--- src/lib/components/draft/PokedexDisplay.svelte
+++ src/lib/components/draft/PokedexDisplay.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
-	import { Heading, Spinner } from 'flowbite-svelte';
-	import { createSelection } from '../store/pokemonSelectionStore.svelte';
+	// Forms
+	import { enhance } from '$app/forms';
+	// Components
+	import { Button, Heading, Spinner } from 'flowbite-svelte';
+	import PokemonInformationDisplay from '../pokemon/PokemonInformationDisplay.svelte';
 	import PokemonSearch from './PokemonSearch.svelte';
-
 	// Interfaces
+	import type { IPokemonSelection } from '../store/pokemonSelectionStore.svelte';
+
 	interface IPokedexDisplayProps {
 		userActive: boolean;
+		pokemonSelection: IPokemonSelection;
 	}
 
-	const { userActive = true } = $props<IPokedexDisplayProps>();
-
-	// State Variables
-	const pokemonSelection = createSelection();
+	const { pokemonSelection, userActive } = $props<IPokedexDisplayProps>();
 </script>
 
 {#if userActive}
@@ -19,12 +21,30 @@
 		<div
 			class="flex h-[200px] w-full items-center justify-center rounded-t-3xl border-2 border-black bg-primary-700"
 		/>
-		<div class="h-[400px] w-full border-x-2 border-black bg-cyan-300">
-			<div class="my-10 h-[320px] w-full bg-cyan-500 px-12 py-6">
-				<PokemonSearch {pokemonSelection} />
-			</div>
+		<div class="h-[400px] w-full border-x-2 border-black bg-cyan-400 px-12 py-6">
+			<PokemonSearch {pokemonSelection} />
+			<PokemonInformationDisplay {pokemonSelection} />
 		</div>
-		<div class="h-[200px] w-full rounded-b-3xl border-2 border-black bg-primary-700" />
+		<div
+			class="flex h-[200px] w-full items-center justify-center rounded-b-3xl border-2 border-black bg-primary-700"
+		>
+			{#if pokemonSelection.selectedPokemon}
+				<form
+					action="?/draftPokemon"
+					method="POST"
+					use:enhance={({ formData }) => {
+						formData.set('pokemon_data', JSON.stringify(pokemonSelection.selectedPokemon));
+
+						return async () => {
+							pokemonSelection.pokemonName = '';
+							pokemonSelection.selectedPokemon = undefined;
+						};
+					}}
+				>
+					<Button color="dark" pill size="xl" type="submit">DRAFT</Button>
+				</form>
+			{/if}
+		</div>
 	</div>
 {:else}
 	<div class="col-span-6 flex h-full flex-col justify-center">
