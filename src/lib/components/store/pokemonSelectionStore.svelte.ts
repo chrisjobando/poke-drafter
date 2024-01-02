8fd5d062108f3c8d8de5da02be0338e7dfa46e49
Christopher Obando <chrisjobando@gmail.com>
2024-01-02 10:01:06 -0500

    better state management

--- src/lib/components/store/pokemonSelectionStore.svelte.ts
+++ src/lib/components/store/pokemonSelectionStore.svelte.ts
@@ -1,12 +1,26 @@
-export function createSelection() {
-	let selectedPokemon = $state<string>('');
+import type { IPokemon } from '$lib/db/schema/DatabaseTypes';
+
+export interface IPokemonSelection {
+	pokemonName: string;
+	selectedPokemon: IPokemon | undefined;
+}
+
+export function createPokemonSelection(): IPokemonSelection {
+	let pokemonName = $state<string>('');
+	let selectedPokemon = $state<IPokemon | undefined>();
 
 	return {
+		get pokemonName() {
+			return pokemonName;
+		},
 		get selectedPokemon() {
 			return selectedPokemon;
 		},
-		set selectedPokemon(searchQuery: string) {
-			selectedPokemon = searchQuery;
+		set pokemonName(searchQuery: string) {
+			pokemonName = searchQuery;
+		},
+		set selectedPokemon(pokemonData: IPokemon | undefined) {
+			selectedPokemon = pokemonData;
 		}
 	};
 }
